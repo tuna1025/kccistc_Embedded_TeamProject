4e3e3a691df7fee1fb90d69589e18c0858ed6de2
jeminanda <jeminanda96@naver.com>
2026-08-27 11:57:44 +0900

    servo windup guard추가

--- MyApp/hw/driver/myServo.c
+++ MyApp/hw/driver/myServo.c
@@ -3,15 +3,23 @@
 /* 기구 장착 방향에 맞춰 축별로 0(정방향) / 1(역방향) 지정 */
 #define SERVO_PAN_INVERT    1
 #define SERVO_TILT_INVERT   1
-static float servoAngle[2] = {90.0f, 90.0f};
+static float servoAngle[2] = {90.0f, 120.0f};
 static const uint8_t servoInvert[2] = {SERVO_PAN_INVERT, SERVO_TILT_INVERT};
+static const float servoMinDeg[2] = {SERVO_PAN_MIN_DEG, SERVO_TILT_MIN_DEG};
+static const float servoMaxDeg[2] = {SERVO_PAN_MAX_DEG, SERVO_TILT_MAX_DEG};
+static float servoTarget[2] = {90.0f, 120.0f};
 //가동범위 제한 + 역방향 각도 전환
+float servoClampAngle(uint8_t ch, float deg)
+{
+    if (deg < servoMinDeg[ch]) deg = servoMinDeg[ch];
+    if (deg > servoMaxDeg[ch]) deg = servoMaxDeg[ch];
+
+    return deg;
+}
 static uint16_t servoDegToUs(uint8_t ch, float deg)
 {
-    if (deg < 0.0f)                 deg = 0.0f;
-    if (deg > (float)SERVO_MAX_DEG) deg = (float)SERVO_MAX_DEG;
+    deg = servoClampAngle(ch, deg);
 
-    /* 논리 각도 -> 물리 각도 변환은 여기 한 곳에서만 */
     if (servoInvert[ch] != 0)
         deg = (float)SERVO_MAX_DEG - deg;
 
@@ -23,9 +31,11 @@ void servoInit(void)
 {
     HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
     HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
-    //시작 각도 0, 90
+    servoSetAngle(SERVO_PAN,  90.0f);
+    servoSetAngle(SERVO_TILT, 120.0f);
+
     servoSetTarget(SERVO_PAN,  90.0f);
-    servoSetTarget(SERVO_TILT, 0.0f);
+    servoSetTarget(SERVO_TILT, 120.0f);
 }
 //서보모터 설정한 각도로 이동
 void servoSetAngle(uint8_t ch, float deg)
@@ -44,14 +54,10 @@ float servoGetAngle(uint8_t ch)
 {
     return servoAngle[ch];
 }
-static float servoTarget[2] = {90.0f, 90.0f};
 //급발진을 막기 위해 목표 각도 설정
 void servoSetTarget(uint8_t ch, float deg)
 {
-    if (deg < 0.0f)                 deg = 0.0f;
-    if (deg > (float)SERVO_MAX_DEG) deg = (float)SERVO_MAX_DEG;
-
-    servoTarget[ch] = deg;
+    servoTarget[ch] = servoClampAngle(ch, deg);
 }
 //목표 각도로 시행당 2도로 이동(SERVO_STEP_DEG만큼)
 void servoUpdate(void)
--- MyApp/hw/driver/myServo.h
+++ MyApp/hw/driver/myServo.h
@@ -7,6 +7,14 @@
 #define SERVO_PAN    0
 #define SERVO_TILT   1
 #define SERVO_STEP_DEG   2.0f   /* 1회 갱신당 최대 이동 각도 */
+/* 서보의 전체 기계적 범위 (펄스폭 환산 기준, 건드리지 말 것) */
+#define SERVO_MAX_DEG      180
+
+/* 기구 간섭을 피한 실제 허용 가동 범위 */
+#define SERVO_PAN_MIN_DEG    0.0f
+#define SERVO_PAN_MAX_DEG  180.0f
+#define SERVO_TILT_MIN_DEG 100.0f
+#define SERVO_TILT_MAX_DEG 180.0f
 
 #define SERVO_MIN_US   500
 #define SERVO_MAX_US   2500
@@ -18,4 +26,5 @@ float servoGetAngle(uint8_t ch);
 
 void servoSetTarget(uint8_t ch, float deg);
 void servoUpdate(void);
+float servoClampAngle(uint8_t ch, float deg);
 #endif
