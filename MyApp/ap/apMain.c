666e9f02840132ee199f965a1fae55539d63dba7
Ricky Um <rickyum527@gmail.com>
2026-08-27 12:13:05 +0900

    updated branch

--- MyApp/ap/apMain.c
+++ MyApp/ap/apMain.c
@@ -3,21 +3,15 @@
 #include "myServo.h"
 #include "myJoystick.h"
 #include "myLaser.h"
+#include "myLcd1602.h"
 #include "stm32f4xx_hal.h"
+#include "mySsd1306.h"
 #include <stdint.h>
-#include "myPiezo.h"
-#include "stm32f4xx_hal_adc.h"
+
 #define AIM_SPEED_DEG   1.5f    /* 50ms당 최대 이동 각도 */
 static float panAngle  = 90.0f;
-static float tiltAngle = 90.0f;
+static float tiltAngle = 120.0f;
 
-static float aimClamp(float deg)
-{
-    if (deg < 0.0f)                 deg = 0.0f;
-    if (deg > (float)SERVO_MAX_DEG) deg = (float)SERVO_MAX_DEG;
-
-    return deg;
-}
 
 static uint8_t btnPrev = 1;   /* 풀업이라 평소 High */
 
@@ -40,76 +34,79 @@ void apInit(void)
     joystickInit();
     laserInit();
     cdsInit();
+    if (lcd1602Init())
+    {
+        lcd1602Cursor(0, 0);
+        lcd1602Print("SYSTEM READY    ");
+        lcd1602Cursor(1, 0);
+        lcd1602Print("LCD1602 OK      ");
+    }
+
+    if (ssd1306Init())
+    {
+        ssd1306DrawString(22, 28, "SYSTEM READY", SSD1306_COLOR_WHITE);
+        ssd1306Update();
+    }
 }
-volatile uint32_t adc0 = 0;
-volatile uint32_t adc1 = 0;
-volatile uint32_t adc4 = 0;
 
 void apMain(void)
 {
-    //startRhythm();
+    
     uint32_t tPrev20   = 0;
     uint32_t tPrev50   = 0;
     uint32_t tPrev100  = 0;
     uint32_t tPrev250  = 0;
     uint32_t tPrev1000 = 0;
-    //테스트용 코드 (pan 각도 조절)
-    // servoSetTarget(SERVO_PAN,  90.0);
     while (1)
     {
         cdsUpdate();
-        // uint32_t tNow = HAL_GetTick();
-        // if (tNow - tPrev20 >= 20)
-        // {
-        //     tPrev20 = tNow;
+        uint32_t tNow = HAL_GetTick();
+        if (tNow - tPrev20 >= 20)
+        {
+            tPrev20 = tNow;
 
-        //     laserUpdate();
-        // }
-        // /* 50ms : 조이스틱 읽기 -> 서보 각도 갱신 */
-        // if (tNow - tPrev50 >= 50)
-        // {
-        //     tPrev50 = tNow;
+        }
+        /* 50ms : 조이스틱 읽기 -> 서보 각도 갱신 */
+        if (tNow - tPrev50 >= 50)
+        {
+            tPrev50 = tNow;
 
-        //     joystickUpdate();
+            joystickUpdate();
 
-        //     panAngle  += joystickGetRatio(JOY_AXIS_X) * AIM_SPEED_DEG;
-        //     tiltAngle -= joystickGetRatio(JOY_AXIS_Y) * AIM_SPEED_DEG;
+            panAngle  += joystickGetRatio(JOY_AXIS_X) * AIM_SPEED_DEG;
+            tiltAngle -= joystickGetRatio(JOY_AXIS_Y) * AIM_SPEED_DEG;
+            panAngle  = servoClampAngle(SERVO_PAN,  panAngle);
+            tiltAngle = servoClampAngle(SERVO_TILT, tiltAngle);
+            servoSetTarget(SERVO_PAN,  panAngle);
+            servoSetTarget(SERVO_TILT, tiltAngle);
 
-        //     panAngle  = aimClamp(panAngle);
-        //     tiltAngle = aimClamp(tiltAngle);
+            servoUpdate();
+            if (btnFirePressed())
+                laserFire();
+        }
 
-        //     servoSetTarget(SERVO_PAN,  panAngle);
-        //     servoSetTarget(SERVO_TILT, tiltAngle);
+        /* 100ms : 버튼 입력 처리 (시작 스위치 / 발사 버튼) */
+        if (tNow - tPrev100 >= 100)
+        {
+            tPrev100 = tNow;
 
-        //     servoUpdate();
-        //     if (btnFirePressed())
-        //         laserFire();
-        // }
+            /* buttonUpdate();  */
+            /* laserUpdate();   */
+        }
 
-        // /* 100ms : 버튼 입력 처리 (시작 스위치 / 발사 버튼) */
-        // if (tNow - tPrev100 >= 100)
-        // {
-        //     tPrev100 = tNow;
+        /* 250ms : 디스플레이 갱신 */
+        if (tNow - tPrev250 >= 250)
+        {
+            tPrev250 = tNow;
 
-        //     /* buttonUpdate();  */
-        //     /* laserUpdate();   */
-        // }
+            /* displayUpdate(); */
+        }
 
-        // /* 250ms : 디스플레이 갱신 */
-        // if (tNow - tPrev250 >= 250)
-        // {
-        //     tPrev250 = tNow;
+        /* 1000ms : 상태 확인용 하트비트 */
+        if (tNow - tPrev1000 >= 1000)
+        {
+            tPrev1000 = tNow;
 
-        //     /* displayUpdate(); */
-        // }
-
-        // /* 1000ms : 상태 확인용 하트비트 */
-        // if (tNow - tPrev1000 >= 1000)
-        // {
-        //     tPrev1000 = tNow;
-
-        //     //HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
-        // }
-        
+        }
     }
 }
